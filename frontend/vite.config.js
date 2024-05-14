9080c71ad18f0f0896cb0531af865454a88decde
Kiho <kiho_hitomi@gdo.co.jp>
2024-05-14 04:57:58 +0000

    Update title and color

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,19 @@
+import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+ 
+import { createVuePlugin as vue } from "vite-plugin-vue2";
+ 
+const path = require("path");
+ 
+const codespaceName = process.env['CODESPACE_NAME'];
+const codespaceDomain = process.env['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN'];
+const hmrPort = 5173;
+ 
+const hmrRemoteHost = codespaceName ? `${codespaceName}-${hmrPort}.${codespaceDomain}` : 'localhost';
+const hmrRemotePort = codespaceName ? 443 : hmrPort;
+const hmrRemoteProtocol = codespaceName ? 'wss' : 'ws';
+ 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
